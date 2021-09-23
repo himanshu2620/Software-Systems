875f46acc2e28b1e474b62ace5b6aa99c6832b5d
himanshu2620 <himanshu.ty45@gmail.com>
2021-09-23 23:13:53 +0530

    qn16 done

---
+++ Hands_On_1/16b.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// compile as gcc 16b.c
+// run the program as ./a.out srcfile
+
+void main(int argc, char *argv[]) {
+  if(argc==2){
+      char* srcFile = argv[1];
+      int fd = open(srcFile, O_RDWR);
+      if(fd==-1){
+        printf("Error in opening the file\n");
+      }
+      else{
+        struct flock curr;
+        curr.l_type = F_RDLCK;
+        curr.l_whence = SEEK_SET;
+        curr.l_start = 0;
+        curr.l_len =0; // 0 is to lock all bytes
+        curr.l_pid = getpid();
+        fcntl(fd,F_SETLK,&curr);
+        printf("Read lock has been applied to file\n");
+      }
+  }
+  else{
+    printf("Pass correct arguments\n");
+  }
+}
