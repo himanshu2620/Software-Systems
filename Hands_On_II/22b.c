587aca8142aa4aec901267b662255526f0637baf
himanshu2620 <himanshu.ty45@gmail.com>
2021-10-13 12:59:16 +0530

    qn 33 done

---
+++ Hands_On_II/22b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+// run as cc -o write 20a.c
+// then   cc -o read 20b.c
+// then ./write
+// then ./read
+void main(){
+    int currFd = open("myFifo_22",O_RDONLY);
+    if(currFd==-1){
+      printf("Error couldn't open file properly\n");
+    }
+    else{
+      char buff[11];
+      read(currFd,buff,sizeof(buff));
+      printf("Message Read from FIFO file is : %s\n", buff);
+      close(currFd);
+    }
+  }
