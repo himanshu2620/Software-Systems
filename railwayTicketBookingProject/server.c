da7c9cf1a99412032b7e398a97251ec266f19df1
himanshu2620 <himanshu.ty45@gmail.com>
2021-10-25 10:06:59 +0530

    starting ticketBooking project

---
+++ railwayTicketBookingProject/server.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+// run as cc -o cli lient.c
+// then   cc -o serv server.c
+// then ./serv
+// then ./cli in a new terminal
+void main(){
+    struct sockaddr_in serv;
+    struct sockaddr_in cli;
+    int sfd = socket(AF_INET,SOCK_STREAM,0);
+    if(sfd==-1){
+       printf("Error in creating socket\n");
+    }
+    else{
+      serv.sin_family = AF_INET;
+      serv.sin_addr.s_addr = INADDR_ANY;
+      serv.sin_port = htons(8089);
+      bind(sfd,(struct sockaddr *) &serv,sizeof(serv));
+      listen(sfd,5);
+      while(1){
+        int clientSize =  sizeof(cli);
+        int nsd = accept(sfd,(struct sockaddr *) &cli, &clientSize);
+        pid_t pid = fork();
+        if(pid==0){
+          write(nsd,"Hello from Server",18);
+          printf("Data sent to Client from server\n");
+          char data[20];
+          read(nsd,data,20);
+          printf("Data received from Client is : %s\n", data);
+        }
+        else{
+          close(nsd);
+        }
+      }
+      close(sfd);
+    }
+}
---
+++ railwayTicketBookingProject/user.h
@@ -0,0 +1,7 @@
+struct user{
+  int userId;
+  char userName[30];
+  char password[30];
+  char bookings[10][30];
+  int count;
+}
